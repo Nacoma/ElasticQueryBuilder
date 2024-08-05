228a5e18557deb2c5600e9c524c5a8ed2eb27e06
Cody Mann <cody@800.com>
2024-08-05 11:19:18 -0500

    add missing features to spatie impl

--- examples/spatie.php
+++ examples/spatie.php
@@ -1,7 +1,10 @@
 <?php
 
 use Carbon\CarbonImmutable;
+use Spatie\ElasticsearchQueryBuilder\Aggregations\Aggregation;
 use Spatie\ElasticsearchQueryBuilder\Aggregations\CardinalityAggregation;
+use Spatie\ElasticsearchQueryBuilder\Aggregations\Concerns\WithAggregations;
+use Spatie\ElasticsearchQueryBuilder\Aggregations\Concerns\WithMissing;
 use Spatie\ElasticsearchQueryBuilder\Aggregations\SumAggregation;
 use Spatie\ElasticsearchQueryBuilder\Aggregations\TermsAggregation;
 use Spatie\ElasticsearchQueryBuilder\Builder;
@@ -10,11 +13,14 @@ use Spatie\ElasticsearchQueryBuilder\Queries\RangeQuery;
 use Spatie\ElasticsearchQueryBuilder\Queries\TermQuery;
 use Spatie\ElasticsearchQueryBuilder\Queries\TermsQuery;
 
+/**
+ * Some of the aggregations that we use aren't implemented by spatie. Their implementations are at the bottom.
+ */
+
 $client = new Builder(/** elastic client builder not installed in this repo */);
 
 $start = CarbonImmutable::now();
 $end = CarbonImmutable::now()->subYear();
-
 
 $client->addQuery(
     BoolQuery::create()
@@ -39,11 +45,93 @@ $client->addAggregation(
         ->aggregation(
             SumAggregation::create('total_revenue', 'revenue')
         )
-        // avg aggregation not implemented
+        ->aggregation(
+        // avg aggregation not implemented by package
+            AvgAggregation::create('average_revenue', 'revenue')
+        )
         ->aggregation(
         // precision threshold not supported
             CardinalityAggregation::create('num_unique_products_purchased', 'sku')
         )
 );
 
-//date histogram not supported.
+$client->addAggregation(
+    DateHistogram::create('by_day', '@timestamp', '1d')
+        ->aggregation(SumAggregation::create('daily_revenue', 'revenue'))
+);
+
+
+/**
+ * Not implemented by Spatie
+ */
+class AvgAggregation extends Aggregation
+{
+    use WithMissing;
+
+    protected string $field;
+    public function __construct(string $name, string $field)
+    {
+        $this->name = $name;
+        $this->field = $field;
+    }
+
+    public static function create(string $name, string $field): self
+    {
+        return new self($name, $field);
+    }
+
+    public function payload(): array
+    {
+        $parameters = [
+            'field' => $this->field,
+        ];
+
+        if ($this->missing) {
+            $parameters['missing'] = $this->missing;
+        }
+
+        return [
+            'sum' => $parameters,
+        ];
+    }
+}
+
+/**
+ * Not implemented by Spatie.
+ */
+class DateHistogram extends Aggregation
+{
+    use WithAggregations;
+
+    protected string $field;
+    protected string $interval;
+
+    public function __construct(string $name, string $field, string $interval)
+    {
+        $this->name = $name;
+        $this->field = $field;
+        $this->interval = $interval;
+    }
+
+    public static function create(string $name, string $field, string $interval): self
+    {
+        return new self($name, $field, $interval);
+    }
+
+    public function payload(): array
+    {
+        $parameters = [
+            'date_histogram' => [
+                'field' => $this->field,
+                'calendar_interval' => $this->interval,
+            ],
+        ];
+
+        if (!$this->aggregations->isEmpty()) {
+            $parameters['aggregations'] = $this->aggregations->toArray();
+        }
+
+        return $parameters;
+    }
+}
+
